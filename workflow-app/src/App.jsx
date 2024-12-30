7faced62eb5427c4e19b504c71ea1b79e83f7852
Your Name <your.email@example.com>
2024-12-30 03:26:47 +0300

    del-header

--- workflow-app/src/App.jsx
+++ workflow-app/src/App.jsx
@@ -1,17 +1,11 @@
 import Sidebar from "./Components/Sidebar";
-import Header from "./components/Header";
 import "./App.css";
 
 function App() {
   return (
     <div className="app">
-      <Header />
-      <div className="content-wrapper">
-        <Sidebar />
-        <main className="main-content">
-          {/* Your main content goes here */}
-        </main>
-      </div>
+      <Sidebar />
+      <main className="main-content">{/* Your main content goes here */}</main>
     </div>
   );
 }
