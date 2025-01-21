51c6621bff237dcf03f35d8ca25f65c0c209fe5c
Your Name <your.email@example.com>
2025-01-21 16:30:26 +0300

    search bar cursor change

--- workflow-app/src/Pages/DashboardPage.jsx
+++ workflow-app/src/Pages/DashboardPage.jsx
@@ -31,11 +31,10 @@ const DashboardPage = () => {
 
         <div className="dashboard-card">
           <div className="card-icon">
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
-              <polygon points="12 2 2 7 12 12 22 7 12 2"/>
-              <polyline points="2 17 12 22 22 17"/>
-              <polyline points="2 12 12 17 22 12"/>
-            </svg>
+          <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M6.429 9.75 2.25 12l4.179 2.25m0-4.5 5.571 3 5.571-3m-11.142 0L2.25 7.5 12 2.25l9.75 5.25-4.179 2.25m0 0L21.75 12l-4.179 2.25m0 0 4.179 2.25L12 21.75 2.25 16.5l4.179-2.25m11.142 0-5.571 3-5.571-3" />
+</svg>
+
           </div>
           <div className="card-content">
             <h3>Active Projects</h3>
