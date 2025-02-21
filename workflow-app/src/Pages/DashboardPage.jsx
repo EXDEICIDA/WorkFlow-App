ba9ee9d3c87f42d15f4a3f6c3c40df6b4027b342
Your Name <your.email@example.com>
2025-02-21 19:03:20 +0300

    icon-edit

--- workflow-app/src/Pages/DashboardPage.jsx
+++ workflow-app/src/Pages/DashboardPage.jsx
@@ -233,17 +233,12 @@ const DashboardPage = () => {
                 New Task
               </button>
               <button className="action-button secondary">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="16"
+              <svg xmlns="http://www.w3.org/2000/svg"  width="16"
                   height="16"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  stroke="currentColor"
-                  strokeWidth="2"
-                >
-                  <polygon points="12 2 2 7 12 12 22 7 12 2" />
-                </svg>
+               fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 14.25v-2.625a3.375 3.375 0 0 0-3.375-3.375h-1.5A1.125 1.125 0 0 1 13.5 7.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H8.25m6.75 12-3-3m0 0-3 3m3-3v6m-1.5-15H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 0 0-9-9Z" />
+</svg>
+
                 New Project
               </button>
               <button className="action-button tertiary">
