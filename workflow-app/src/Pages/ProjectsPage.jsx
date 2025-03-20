09a8359a7cf09d0b85de1644a6e9923c88fecf63
Your Name <your.email@example.com>
2025-03-21 02:19:23 +0300

    search bar icon

--- workflow-app/src/Pages/ProjectsPage.jsx
+++ workflow-app/src/Pages/ProjectsPage.jsx
@@ -153,18 +153,19 @@ const ProjectsPage = () => {
         <div className="projects-header-actions">
           <div className="search-bar">
             <svg 
-              className="search-icon" 
+              className="search-icon-left"
               xmlns="http://www.w3.org/2000/svg" 
-              fill="none" 
+              width="18" 
+              height="18" 
               viewBox="0 0 24 24" 
+              fill="none" 
+              stroke="currentColor" 
               strokeWidth="1.5" 
-              stroke="currentColor"
+              strokeLinecap="round" 
+              strokeLinejoin="round"
             >
-              <path 
-                strokeLinecap="round" 
-                strokeLinejoin="round" 
-                d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" 
-              />
+              <circle cx="11" cy="11" r="8"></circle>
+              <line x1="21" y1="21" x2="16.65" y2="16.65"></line>
             </svg>
             <input
               type="text"
